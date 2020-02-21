90025ed94d929b3e92663362af7a5691c4edeadc
hasnainalimj <mjhasnain15@gmail.com>
2020-02-21 05:53:24 +0500

    js class 8

---
+++ Javascript/Class 8/js/app.js
@@ -0,0 +1,76 @@
+//For Loop Flaging
+
+var countries = ['pak' , 'ind' , 'rsa','sri' , 'ban' , 'aus' , 'ind']
+// var input = prompt("Enter you country"); //ban
+
+// var found = false; //flag
+
+// for(var i=0; i<countries.length; i++){
+//     // console.log("TCL: countries[i]", countries[i])
+//     if(countries[i] == input){
+//         found = true
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// if(found == true){
+//     console.log("mil gaya")
+// }else{
+//     console.log("nh mila")
+// }
+
+//FOR LOOP NESTED
+
+// for(var i=0; i<10; i++){
+//     console.log(i) //0 1 2 ... 9
+//     for(var j=0; j<5; j++){
+//         console.log(j) // 0 1 2 3 4
+//     }
+// }
+
+// for(i = 0; i<3; i++){
+//     for(j=0; j<3; j++){
+//         console.log(i , j)
+//     }
+// }
+
+// for(i = 0; i<3; i++){
+//     for(j=0; j<3; j++){
+//         for(k=0; k<3; k++){
+//             console.log(i , j , k) 
+//         }
+//     }
+// }
+
+// var nestedArray = [
+//     ["A" , "B" , "C"],
+//     ["D" , "E" , "F"]
+// ]
+
+// for(i=0; i<nestedArray.length; i++){
+//     // console.log(nestedArray[i])
+//     for(j=0; j<nestedArray[i].length; j++){
+//         console.log(nestedArray[i][j])
+//     }
+// }
+
+// var firstNames = ['Hasnain' , 'Faizan' , 'Kaleem' , 'Amreen']
+// var lastNames = ['Ali' , 'Pervaiz' , 'Ilyas' , 'Ali']
+// var fullNames = [];
+
+// for(i=0; i<firstNames.length; i++){
+//     fullNames.push(firstNames[i] + " " + lastNames[i])
+// }
+
+// console.log(fullNames)
+
+//CHANGING CASE
+//1 toUpperCase()
+//2 toLowerCase()
+
+// var userInput = prompt("enter any thing")
+
+// console.log(userInput.toUpperCase())
+// console.log(userInput.toLowerCase())
+
