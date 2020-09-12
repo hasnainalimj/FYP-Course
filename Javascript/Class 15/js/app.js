9b4ee049b07d90410493beef50bec1a0cc6ccee2
hasnainalimj <mjhasnain15@gmail.com>
2020-09-12 18:29:51 +0500

    Class 15 for Kaleem

---
+++ Javascript/Class 15/js/app.js
@@ -0,0 +1,95 @@
+// alert(123123123123123) //Number
+// alert('Hello World')  //String
+// window.alert(true) //Boolean
+
+var name = 'Class 15' //Variable for String
+
+// alert(name)
+
+var weight = 200 //Variable for Number
+
+// alert(weight)
+
+var available = true //Variable for Boolean
+
+//Boolean has only two values true 1 & false 0
+
+// alert(available)
+
+var phone //Declaration of variable
+
+phone = '03102932665' //Assigning in variable
+
+// alert(phone) //Using a variable
+
+phone = '090078601' //Reassigning in a variable
+
+// alert(phone)
+
+var num1 = 5
+var result = num1 + 10
+
+// alert(result)
+
+// var function =  //Don't use any js keyword as variable name
+// var 123Int =  //Don't use any number before variable name
+// var num123 = //Correct
+// var @sjsdj  //Don't use some special charectors
+// var $name = 'abc' //Correct
+// var _name = 'Abc' //Correct
+
+// var firstName //Camel Case
+// var fullName //Camel Case
+
+
+//Familiar Operators (+ , - , * , / , %)
+
+var n1 = 5
+var n2 = 5
+var n3 = '2'
+
+// alert(n1 + n2)
+// alert(n1 - n2)
+// alert(n1 * n2)
+// alert(n1 / n2)
+// alert(n1 % n2)
+
+// alert(n1 + n3) //52 Concatenation
+// alert(n1 - n3) //3
+
+
+//Unfamiliar Operators
+// 1) Post Increment ++
+//2) Post Decrement --
+//3) Pre Increment ++
+// 4) Pre Decrement --
+
+//Always executes on number as well as must store in a variable
+//Always increase & decrease 1 
+//Assigning
+
+var int = 10
+
+// alert(int++) //1
+// alert(int--) //2
+// alert(int)
+
+// alert(++int) //3
+// alert(--int) //4
+
+var totalCost = (1 + 3) * 4;
+
+// console.log('ye hy mera result',totalCost)
+
+var firstName = 'Kaleem'
+var lastName = 'Malik'
+
+var fullName = firstName + ' ' + lastName
+
+// console.log(fullName)
+
+// var name = prompt('Enter you name')
+
+// console.log("name", name)
+
+prompt('Enter your umr?' , 25) //with default value
